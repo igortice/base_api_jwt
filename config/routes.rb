2e04b90696d52ad7f9dd2fd2348f103e93510b58
Igor Rocha <igor.rocha@sepof.fortaleza.ce.gov.br>
2020-04-10 20:59:06 -0300

    correct login logout register destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users,
+             path:        'auth',
+             skip:        [:confirmations, :passwords],
+             path_names:  {
+               sign_in:      'login',
+               sign_out:     'logout',
+               registration: 'register'
+             },
+             controllers: {
+               sessions:      'api/v1/users/sessions',
+               registrations: 'api/v1/users/registrations'
+             }
   namespace :api do
     namespace :v1 do
-      devise_for :users,
-                 path:        'auth',
-                 skip:        [:confirmations, :passwords],
-                 sign_in_via: [:put],
-                 path_names:  {
-                   sign_in:      'login',
-                   sign_out:     'logout',
-                   registration: 'register'
-                 },
-                 controllers: {
-                   sessions:      'api/v1/users/sessions',
-                   registrations: 'api/v1/users/registrations'
-                 }
     end
   end
 end
