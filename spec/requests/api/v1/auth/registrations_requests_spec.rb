a1d30b1dde528e85deaf9ff8fe23a184d96612ae
Igor Rocha <igortice@gmail.com>
2020-04-21 09:43:39 -0300

    rspec init tests

--- spec/requests/api/v1/auth/registrations_requests_spec.rb
+++ spec/requests/api/v1/auth/registrations_requests_spec.rb
@@ -1,18 +1,34 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'API::V1::Auth::RegistrationsRequests', type: :request do
   describe 'POST /auth/register' do
     let(:url) { '/auth/register' }
+    let(:params) do
+      {
+        user: {
+          email:    'user@example.com',
+          password: 'password'
+        }
+      }
+    end
+
+    context 'quando usuario não existe' do
+      before { post url, params: params }
+
+      it 'returns 200' do
+        expect(response.status).to eq 200
+      end
+
+      it 'returns new user body' do
+        expect(JSON.parse(response.body)).to eq User.last.as_json
+      end
+    end
 
     context 'quando usuario já existe' do
       before do
-        user   = Fabricate(:user)
-        params = {
-          user: {
-            email:    user.email,
-            password: user.password
-          }
-        }
+        Fabricate :user, email: params[:user][:email]
         post url, params: params
       end
 
